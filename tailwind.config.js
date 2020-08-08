759833c379ca2f3f0a6ca66f182b3382e1ee0591
Alvaro Saburido <alvaro.saburido@gmail.com>
2020-08-08 17:30:30 +0200

    feature(styles): added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ['Montserrat'],
+      sans: ['Roboto', '-apple-system', 'BlinkMacSystemFont'],
+      serif: ['Montserrat', 'Georgia', 'Cambria']
+    },
+    extend: {
+      colors: {
+        valencia: {
+          50: '#FDF6F7',
+          100: '#FBEEEF',
+          200: '#F4D3D7',
+          300: '#EEB9BF',
+          400: '#E18590',
+          500: '#D45060',
+          600: '#BF4856',
+          700: '#7F303A',
+          800: '#5F242B',
+          900: '#40181D'
+        },
+        eastbay: {
+          50: '#F6F7F8',
+          100: '#EDEFF1',
+          200: '#D3D6DC',
+          300: '#B9BDC7',
+          400: '#848C9D',
+          500: '#4F5A73',
+          600: '#475168',
+          700: '#2F3645',
+          800: '#242934',
+          900: '#181B23'
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
